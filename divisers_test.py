bf658574d7f34a5450c476a59cb9e5bc3e143dc4
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-03-08 16:05:20 +0000

    test: GREEN refactored tests to check up to 40

--- divisers_test.py
+++ divisers_test.py
@@ -1,8 +1,5 @@
 from divisers import diviser
-
-def test_returns_positive_integer():
-    assert diviser(1) == 1
-
+'''
 def test_correct_when_one():
     assert diviser(1) == 1
 
@@ -20,3 +17,26 @@ def test_correct_when_five():
 
 def test_correct_when_six():
     assert diviser(6) == 4 # 6, 3, 2, 1 
+    
+def test_correct_when_seven():
+    assert diviser(7) == 2 # 7, 1
+    
+def test_correct_when_eight():
+    assert diviser(8) == 4 # 8, 4, 2, 1
+    
+def test_correct_when_nine():
+    assert diviser(9) == 3 # 9, 3, 1
+'''
+def test_returns_positive_integer():
+    assert diviser(1) == 1
+
+
+
+def test_range_up_to_forty():
+      
+    for i in range(1, 41):
+        number_return = 0
+        for j in range(1,10):
+            if i % j == 0:
+                number_return += 1
+        assert diviser(i) == number_return
