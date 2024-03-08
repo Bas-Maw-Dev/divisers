53c0804490d7ad30a651dfdabb312d058e6d3353
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-03-08 14:06:13 +0000

    test: RED incorrect answer when 5 tested

--- divisers_test.py
+++ divisers_test.py
@@ -2,7 +2,7 @@ from divisers import diviser
 
 def test_returns_positive_integer():
     assert diviser(1) == 1
-    
+
 def test_correct_when_one():
     assert diviser(1) == 1
 
@@ -11,6 +11,9 @@ def test_correct_when_two():
 
 def test_correct_when_three():
     assert diviser(3) == 2  # 3, 1
-    
+
 def test_correct_when_four():
     assert diviser(4) == 3  # 4, 2, 1
+
+def test_correct_when_five():
+    assert diviser(5) == 2 # 5, 1
