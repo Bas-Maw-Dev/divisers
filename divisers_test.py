82ebcb9ea3db00c6c5eeb5d984c48103afe6c1d1
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-03-08 13:46:29 +0000

    test: RED - 3 returns wrong answer

--- divisers_test.py
+++ divisers_test.py
@@ -2,3 +2,12 @@ from divisers import diviser
 
 def test_returns_positive_integer():
     assert diviser(1) == 1
+    
+def test_correct_when_one():
+    assert diviser(1) == 1
+
+def test_correct_when_two():
+    assert diviser(2) == 2
+
+def test_correct_when_three():
+    assert diviser(3) == 1
