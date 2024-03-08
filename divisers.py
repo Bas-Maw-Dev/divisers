26be39c9501a7f716cb0788423c87bdb7099ce8f
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-03-08 15:31:22 +0000

    refactor: GREEN - made range cover all numbers from 1 - 9

--- divisers.py
+++ divisers.py
@@ -1,6 +1,6 @@
 def diviser(n):
     number_of_divisers = 0
-    for i in range(1, 7):
+    for i in range(1, 10):
         if n % i == 0:
             number_of_divisers += 1
     return number_of_divisers
