eab60eb211fe79fd58bfc91696914bafa9a51a07
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-03-08 16:14:37 +0000

    refactor: created a more succint function using sum to count numbers which work.

--- divisers.py
+++ divisers.py
@@ -1,6 +1,2 @@
 def diviser(n):
-    number_of_divisers = 0
-    for i in range(1, 10):
-        if n % i == 0:
-            number_of_divisers += 1
-    return number_of_divisers
+    return sum(n % i == 0 for i in range(1, 10))
